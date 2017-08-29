ff686b165abf55c164c8ab5049cb751d85203da1
lf- <lf-@users.noreply.github.com>
2017-08-28 21:33:44 -0700

    Set up steps/mm for ACME leadscrews and E3D Titan

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -495,7 +495,7 @@
  * Override with M92
  *                                      X, Y, Z, E0 [, E1[, E2[, E3[, E4]]]]
  */
-#define DEFAULT_AXIS_STEPS_PER_UNIT   { 80, 80, 4000, 500 }
+#define DEFAULT_AXIS_STEPS_PER_UNIT   { 80.19, 80.19, 800, 418.5 }
 
 /**
  * Default Max Feed Rate (mm/s)
